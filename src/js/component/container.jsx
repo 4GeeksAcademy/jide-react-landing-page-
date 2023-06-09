c7102405d51499aff49676b6ea650c268f41dc31
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2023-06-09 19:05:17 +0000

    navbar container and jumbotron done

--- src/js/component/container.jsx
+++ src/js/component/container.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Container = ({ children }) => {
+export const Container = ({ children }) => {
     return (
         <div
             style={{
@@ -13,5 +13,3 @@ const Container = ({ children }) => {
         </div>
     );
 };
-
-export default Container;
--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
-const Jumbotron = () => {
+export const Jumbotron = () => {
     return (
-        <div className="p-5 mb-4 bg-body-tertiary rounded-3">
-            <div className="container-fluid py-5">
-                <h1 className="display-5 fw-bold">A Warm Welcome!</h1>
-                <p className="col-md-8 fs-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lacus id tortor tempus luctus vitae quis purus. Donec nisi est, blandit id ante nec, dapibus feugiat est.</p>
-                <button className="btn btn-primary btn-lg" type="button">Call to action!</button>
+        <div className="p-2 mb-2 pb-5 bg-light rounded-2">
+            <div className="container-fluid py-1">
+                <h1 className="display-5 fs-2">A Warm Welcome!</h1>
+                <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed pharetra lacus id tortor tempus luctus vitae quis purus. Donec nisi est, blandit id ante nec, dapibus feugiat est.</p>
+                <button className="btn btn-primary btn" type="button">Call to action!</button>
             </div>
         </div>
     );
 };
-
-export default Jumbotron;
